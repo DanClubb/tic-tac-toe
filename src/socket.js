fafe3ddefba2683a09837ec20e15dd6337e46cde
daniel-clubb <danclubb004@gmail.com>
2023-11-02 10:24:15 +0000

    swapped cors origin

--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-// const socket = io("https://tic-tac-toe-6mi5.onrender.com/");
-const socket = io("http://localhost:8080");
+const socket = io("https://tic-tac-toe-6mi5.onrender.com/");
+// const socket = io("http://localhost:8080");
 
 export default socket;
